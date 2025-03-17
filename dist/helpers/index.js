c6a2f505467ead99c7382500cbda2a899f5197bd
Pogran PC <freelancerphpcss@gmail.com>
2025-03-17 14:52:12 +0300

    drop dist folder

--- dist/helpers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.devLaunchOptions = void 0;
-exports.devLaunchOptions = {
-    headless: false,
-    slowMo: 50,
-    defaultViewport: null
-};
